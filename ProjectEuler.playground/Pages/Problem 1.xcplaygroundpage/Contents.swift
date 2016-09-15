f0f7c30713d46266127bf536e08de695b085b0e7
MohamedDiaa <mohameddiaa1988@hotmail.com>
2016-09-15 19:30:21 +0200

    Simple brutal force for Solution1

--- ProjectEuler.playground/Pages/Problem 1.xcplaygroundpage/Contents.swift
+++ ProjectEuler.playground/Pages/Problem 1.xcplaygroundpage/Contents.swift
@@ -1,5 +1,20 @@
 //: [Previous](@previous)
 
 import Foundation
+
 //https://projecteuler.net/problem=1
 
+//If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+//Find the sum of all the multiples of 3 or 5 below 1000.
+
+var sum = 0
+var max = 1000
+
+for index in 1..<1000{
+
+    if (index%3 == 0) || (index%5 == 0){
+        sum += index
+        //print(index)
+    }
+}
+print(sum)
