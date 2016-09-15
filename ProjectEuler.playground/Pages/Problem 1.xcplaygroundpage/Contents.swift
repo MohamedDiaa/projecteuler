d2f32c6b731658046a360988554c507ec7efd7ea
MohamedDiaa <mohameddiaa1988@hotmail.com>
2016-09-15 18:13:27 +0200

    First commit

---
+++ ProjectEuler.playground/Pages/Problem 1.xcplaygroundpage/Contents.swift
@@ -0,0 +1,5 @@
+//: [Previous](@previous)
+
+import Foundation
+//https://projecteuler.net/problem=1
+
---
+++ ProjectEuler.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -0,0 +1,28 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+var str = "Hello, playground"
+
+//Problem 43
+//SubString Divisibility 
+
+
+//PanDigital numbers 
+
+let panDigitalCount = 9*8*7*6*5*4*3*2*1
+
+// [1] [2] [3] [4] [5] [6] [7] [8] [9]
+var indexSet = NSIndexSet(indexesInRange: NSRange(location: 1, length: 9))
+print(indexSet)
+
+//The idea is to be more couage when dealing with DataStructures , loops ,computations theses all tools are toys in your toes.
+
+
+
+let date1 = NSDate()
+//cycle()
+let date2 = NSDate()
+
+let timeInterval = date1.timeIntervalSinceDate(date2)
+print(timeInterval/(60))
